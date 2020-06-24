b93a3ededabac1af0f76d5a6e59638d792b3db50
DESKTOP-GUJB72J\Tyler <Tyler.martinez89@gmail.com>
2020-06-24 16:23:34 -0400

    Serenity added, ping for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg:Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+                println!("Errored sending message: {}", why)
+            }
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let token = env::var("DISCORD_TOKEN")
+        .expect("No token found!");
+    
+        let mut client = Client::new(&token, Handler).expect("Issue creating client!");
+
+
+        if let Err(why) = client.start() {
+            println!("Client error: {:?}", why);
+        }
 }
