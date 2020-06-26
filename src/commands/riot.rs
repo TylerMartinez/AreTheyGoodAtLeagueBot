9f3aed14db2db2846c3b96e2de3824eefe13f17a
Tyler Martinez <tyler.martinez89@gmail.com>
2020-06-25 21:09:25 -0400

    Starting adding JSON models, getting account id and match list from riot

--- src/commands/riot.rs
+++ src/commands/riot.rs
@@ -1,8 +1,58 @@
+use serde::Deserialize;
 
-pub fn get_riot_url() -> String {
+// Models
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+pub struct Summoner {
+    pub accountId: String,
+    pub profileIconId: i32,
+    pub revisionDate: i64,
+    pub name: String,
+    pub id: String,
+    pub puuid: String,
+    pub summonerLevel: i64,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+pub struct MatchList {
+    pub startIndex: i32,
+    pub totalGames: i32,
+    pub endIndex: i32,
+    pub matches: Vec<MatchReference>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+pub struct MatchReference {
+    pub gameId: i64,
+    pub role: String,
+    pub season: i32,
+    pub platformId: String,
+    pub champion: i32,
+    pub queue: i32,
+    pub lane: String,
+    pub timestamp: i64,
+}
+
+// Functions
+pub fn get_riot_url(
+    region: &str,
+    service: &str,
+    endpoint: &str,
+    resource: &str,
+    query: &mut String,
+) -> String {
     // Get token
-    let riot_token = std::env::var("RIOT_TOKEN")
-        .expect("No token found!");
-    
-    format!("https://{}.api.riotgames.com/lol/{}/{}/{}?api_key={}", "na1", "summoner/v4/summoners", "by-name", "robotgtar", riot_token)
+    let riot_token = std::env::var("RIOT_TOKEN").expect("No token found!");
+
+    // Add additional format if query has a value
+    if query.len() > 0 {
+        query.push('&');
+    }
+
+    format!(
+        "https://{}.api.riotgames.com/lol/{}/{}/{}?{}api_key={}",
+        region, service, endpoint, resource, query, riot_token
+    )
 }
--- src/commands/verdict.rs
+++ src/commands/verdict.rs
@@ -1,34 +1,59 @@
-use serenity::prelude::*;
+use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::*;
-use serenity::framework::standard::{
-    CommandResult,
-    macros::command,
-};
+use serenity::prelude::*;
 
 use crate::commands::riot;
 
 use reqwest;
 
-
 #[command]
-fn verdict(ctx: &mut Context, msg: &Message) -> CommandResult {
+fn verdict(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
     // Start Message
-    let _ = msg.channel_id.say(&ctx.http, "Trying to GET...");
+    let _ = msg.channel_id.say(
+        &ctx.http,
+        "Give me a second while I look this summoner up...",
+    );
 
-    // Build Url 
-    let url:String = riot::get_riot_url();
-
-    let _ = msg.channel_id.say(&ctx.http, format!("..to this url: {}...", url));
-
+    // Intialize a blocking client for now
     let client = reqwest::blocking::Client::new();
 
-    let response = client.get(reqwest::Url::parse(&url[..]).unwrap())
+    // Build Summonner info Url and the get it
+    let url: String = riot::get_riot_url(
+        "na1",
+        "summoner/v4/summoners",
+        "by-name",
+        args.rest(),
+        &mut "".into(),
+    );
+
+    let summoner_info = client
+        .get(reqwest::Url::parse(&url[..]).unwrap())
         .send()?
-        .text()?;
+        .json::<riot::Summoner>()?;
 
-    let _ = msg.channel_id.say(&ctx.http, "..and got:");
+    // Build Match infor url and get it
+    let url: String = riot::get_riot_url(
+        "na1",
+        "match/v4/matchlists",
+        "by-account",
+        &summoner_info.accountId[..],
+        &mut "endIndex=15".into(),
+    );
 
-    let _ = msg.channel_id.say(&ctx.http, response);
+    let match_list = client
+        .get(reqwest::Url::parse(&url[..]).unwrap())
+        .send()?
+        .json::<riot::MatchList>()?;
+
+    let _ = msg.channel_id.say(
+        &ctx.http,
+        format!(
+            "Alright I have here their last {} matches. Let's see how they did...",
+            match_list.matches.len()
+        ),
+    );
+
+    // Lets get the individual match data to pull more insight
 
     Ok(())
 }
